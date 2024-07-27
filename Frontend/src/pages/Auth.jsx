8bad9acbf2133273a9b736891133c6a6fb2b4d21
AimanAzmi18 <aimanazmi1816@gmail.com>
2024-07-27 21:29:51 +0800

    fix

--- Frontend/src/pages/Auth.jsx
+++ Frontend/src/pages/Auth.jsx
@@ -2,11 +2,9 @@ import ToggleDiv from '../components/Toggle';
 
 const Auth = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-full min-h-screen bg-custom-gradient p-4 sm:p-8">
-      <h1 className='text-black bg-box-color m-6 p-4 border-black border-2 text-center'>
-        Welcome to the land of authentication, where you get to prove you’re a real person. How exciting
-      </h1>
-      <div className="w-full max-w-screen-lg h-full sm:h-2/3 overflow-hidden border-black border-4">
+    <div className="flex flex-col items-center justify-center w-full h-screen bg-custom-gradient">
+      <h1 className='text-black bg-box-color m-6 p-4 border-black border-2'>Welcome to the land of authentication, where you get to prove you’re a real person. How exciting</h1>
+      <div className="max-w-screen-lg w-full h-2/3 overflow-hidden border-black border-4">
         <ToggleDiv />
       </div>
     </div>
